f635a5cd03f89f9cda98631a3ab6e0d1942053b7
zgalusic <zgalusic@tis.hr>
2023-09-06 22:46:14 +0200

    implement product service

--- business/src/main/java/com/ingemark/business/service/ProductServiceImpl.java
+++ business/src/main/java/com/ingemark/business/service/ProductServiceImpl.java
@@ -2,9 +2,13 @@ package com.ingemark.business.service;
 
 import com.ingemark.business.data.entity.Product;
 import com.ingemark.business.data.repository.ProductRepository;
+import com.ingemark.business.service.mapper.ProductMapper;
 import com.ingemark.dto.ProductDto;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,33 +25,74 @@ public class ProductServiceImpl implements ProductService {
     public ProductDto fetch(long id) {
 
         Optional<Product> productOptional = productRepository.findById(id);
+        ProductDto productDto = null;
+        if(productOptional.isPresent()){
+            productDto = ProductMapper.map(productOptional.get());
+        }
 
-        return null;
+        return productDto;
     }
 
     @Override
     public List<ProductDto> fetchAll() {
 
-        Iterable<Product> productList = productRepository.findAll();
+        Iterator<Product> productIterator = productRepository.findAll().iterator();
+        List<ProductDto> productDtoList = new ArrayList<>();
 
-        return null;
+        while(productIterator.hasNext()) {
+            Product product = productIterator.next();
+            ProductDto productDto = ProductMapper.map(product);
+            productDtoList.add(productDto);
+        }
+
+        return productDtoList;
     }
 
     @Override
     public List<ProductDto> filterBy(ProductDto productDto) {
+
+        // TODO
+
         return null;
     }
 
     @Override
     public long create(ProductDto productDto) {
-        return 0;
+        Product product = ProductMapper.map(productDto);
+        Product savedProduct = productRepository.save(product);
+        return savedProduct.getId();
     }
 
     @Override
     public void update(ProductDto productDto) {
 
         Optional<Product> productOptional = productRepository.findById(productDto.getId());
-        productOptional.ifPresent(p -> {}); // TODO
+        productOptional.ifPresent(p -> {
+
+            if(productDto.getPriceUsd() != null) {
+                p.setPriceUsd(productDto.getPriceUsd());
+            }
+
+            if(StringUtils.isNotBlank(productDto.getDescription())) {
+                p.setDescription(productDto.getDescription());
+            }
+
+            if(productDto.getPriceEur() != null) {
+                p.setPriceEur(productDto.getPriceEur());
+            }
+
+            if(productDto.getAvailable() != null) {
+                p.setAvailable(productDto.getAvailable());
+            }
+
+            if(StringUtils.isNotBlank(productDto.getName())) {
+                p.setName(productDto.getName());
+            }
+
+            if(StringUtils.isNotBlank(productDto.getCode())) {
+                p.setCode(productDto.getCode());
+            }
+        });
     }
 
     @Override
