c6b64d03ea03ea139396ed43bc4781989bfd7ffb
zgalusic <zgalusic@tis.hr>
2023-09-06 19:14:35 +0200

    create abstract REST client

---
+++ business/src/main/java/com/ingemark/business/api/rest/client/RestClient.java
@@ -0,0 +1,30 @@
+package com.ingemark.business.api.rest.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.client.RestTemplate;
+
+public abstract class RestClient {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RestClient.class);
+
+    private final RestTemplate restTemplate;
+
+    protected RestClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    protected <U> U executeGet(String url, Class<U> clazz) {
+
+        LOGGER.info("HTTP GET {}", url);
+
+        try {
+
+            return restTemplate.getForObject(url, clazz);
+
+        } catch (Exception e) {
+            // TODO create specific exception
+            throw e;
+        }
+    }
+}
