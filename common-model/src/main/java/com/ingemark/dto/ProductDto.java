e5d1a9482e4d4df466db395595cfe9af59df3cfe
zgalusic <zgalusic@tis.hr>
2023-09-06 22:04:12 +0200

    create common-model module

---
+++ common-model/src/main/java/com/ingemark/dto/ProductDto.java
@@ -0,0 +1,72 @@
+package com.ingemark.dto;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class ProductDto implements Serializable {
+
+    private static final long serialVersionUID = 628774163662247291L;
+    private Long id;
+    private String code;
+    private String name;
+    private BigDecimal priceEur;
+    private BigDecimal priceUsd;
+    private String description;
+    private Boolean isAvailable;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPriceEur() {
+        return priceEur;
+    }
+
+    public void setPriceEur(BigDecimal priceEur) {
+        this.priceEur = priceEur;
+    }
+
+    public BigDecimal getPriceUsd() {
+        return priceUsd;
+    }
+
+    public void setPriceUsd(BigDecimal priceUsd) {
+        this.priceUsd = priceUsd;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(Boolean available) {
+        isAvailable = available;
+    }
+}
---
+++ common-model/src/main/java/com/ingemark/wrapper/AbstractWrapper.java
@@ -0,0 +1,14 @@
+package com.ingemark.wrapper;
+
+public abstract class AbstractWrapper {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
