594e968b87bc37e28d2e56b76852138fed936e6a
zgalusic <zgalusic@tis.hr>
2023-09-07 18:01:32 +0200

    update database config

--- business/src/main/java/com/ingemark/business/config/DataSourceConfig.java
+++ business/src/main/java/com/ingemark/business/config/DataSourceConfig.java
@@ -8,37 +8,28 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
+@Profile("develop")
 @Configuration
 public class DataSourceConfig {
 
     @Value("${spring.datasource.url}")
     private String datasourceUrl;
-
+    @Value("${spring.datasource.driverClassName}")
+    private String datasourceDriverClassName;
     @Value("${spring.datasource.username}")
     private String datasourceUsername;
-
     @Value("${spring.datasource.password}")
     private String datasourcePassword;
 
-    @Profile("local")
-    @Bean
-    public DataSource localDataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setUrl(datasourceUrl);
-        dataSource.setUsername(datasourceUsername);
-        dataSource.setPassword(datasourcePassword);
-        return dataSource;
-    }
-
-    @Profile("develop")
     @Bean
     public DataSource dataSource() {
+
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setDriverClassName(datasourceDriverClassName);
         dataSource.setUrl(datasourceUrl);
         dataSource.setUsername(datasourceUsername);
         dataSource.setPassword(datasourcePassword);
+
         return dataSource;
     }
 }
