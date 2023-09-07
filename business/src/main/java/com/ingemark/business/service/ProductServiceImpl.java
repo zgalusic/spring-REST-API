ba3bac59877cb74c0ba5df33fd701540676bd29b
zgalusic <zgalusic@tis.hr>
2023-09-07 11:58:39 +0200

    implement product service

--- business/src/main/java/com/ingemark/business/service/ProductServiceImpl.java
+++ business/src/main/java/com/ingemark/business/service/ProductServiceImpl.java
@@ -1,12 +1,16 @@
 package com.ingemark.business.service;
 
+import com.ingemark.business.api.rest.client.hnb.TecajnaListaRestClientService;
+import com.ingemark.business.api.rest.client.hnb.model.TecajnaListaItem;
 import com.ingemark.business.data.entity.Product;
 import com.ingemark.business.data.repository.ProductRepository;
 import com.ingemark.business.service.mapper.ProductMapper;
 import com.ingemark.dto.ProductDto;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -16,9 +20,11 @@ import java.util.Optional;
 public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository productRepository;
+    private final TecajnaListaRestClientService tecajnaListaRestClientService;
 
-    public ProductServiceImpl(ProductRepository productRepository) {
+    public ProductServiceImpl(ProductRepository productRepository, TecajnaListaRestClientService tecajnaListaRestClientService) {
         this.productRepository = productRepository;
+        this.tecajnaListaRestClientService = tecajnaListaRestClientService;
     }
 
     @Override
@@ -57,48 +63,88 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
+    @Transactional
     public long create(ProductDto productDto) {
-        Product product = ProductMapper.map(productDto);
-        Product savedProduct = productRepository.save(product);
-        return savedProduct.getId();
+
+        try {
+
+            Product product = ProductMapper.map(productDto);
+            TecajnaListaItem[] tecajnaListaItems = tecajnaListaRestClientService.fetch("USD");
+            product.setPriceUsd(convertPrice(product.getPriceEur(), tecajnaListaItems));
+            Product savedProduct = productRepository.save(product);
+
+            return savedProduct.getId();
+
+        } catch (Exception e) {
+
+            throw new ServiceGeneralException(e);
+        }
     }
 
     @Override
+    @Transactional
     public void update(ProductDto productDto) {
 
-        Optional<Product> productOptional = productRepository.findById(productDto.getId());
-        productOptional.ifPresent(p -> {
+        try {
+            Optional<Product> productOptional = productRepository.findById(productDto.getId());
+            productOptional.ifPresent(p -> {
 
-            if(productDto.getPriceUsd() != null) {
-                p.setPriceUsd(productDto.getPriceUsd());
-            }
+                if (StringUtils.isNotBlank(productDto.getDescription())) {
+                    p.setDescription(productDto.getDescription());
+                }
 
-            if(StringUtils.isNotBlank(productDto.getDescription())) {
-                p.setDescription(productDto.getDescription());
-            }
+                if (productDto.getPriceEur() != null) {
+                    p.setPriceEur(productDto.getPriceEur());
+                    TecajnaListaItem[] tecajnaListaItems = tecajnaListaRestClientService.fetch("USD");
+                    p.setPriceUsd(convertPrice(p.getPriceEur(), tecajnaListaItems));
+                }
 
-            if(productDto.getPriceEur() != null) {
-                p.setPriceEur(productDto.getPriceEur());
-            }
+                if (productDto.getAvailable() != null) {
+                    p.setAvailable(productDto.getAvailable());
+                }
 
-            if(productDto.getAvailable() != null) {
-                p.setAvailable(productDto.getAvailable());
-            }
+                if (StringUtils.isNotBlank(productDto.getName())) {
+                    p.setName(productDto.getName());
+                }
 
-            if(StringUtils.isNotBlank(productDto.getName())) {
-                p.setName(productDto.getName());
-            }
+                if (StringUtils.isNotBlank(productDto.getCode())) {
+                    p.setCode(productDto.getCode());
+                }
+            });
 
-            if(StringUtils.isNotBlank(productDto.getCode())) {
-                p.setCode(productDto.getCode());
-            }
-        });
+        } catch (Exception e) {
+
+            throw new ServiceGeneralException(e);
+        }
     }
 
     @Override
+    @Transactional
     public void delete(long id) {
 
-        Optional<Product> productOptional = productRepository.findById(id);
-        productOptional.ifPresent(productRepository::delete);
+        try {
+
+            Optional<Product> productOptional = productRepository.findById(id);
+            productOptional.ifPresent(productRepository::delete);
+
+        } catch (Exception e) {
+
+            throw new ServiceGeneralException(e);
+        }
+    }
+
+    private BigDecimal convertPrice(BigDecimal price, TecajnaListaItem[] tecajnaListaItems) {
+
+        BigDecimal convertedPrice = BigDecimal.ZERO;
+
+        if(tecajnaListaItems != null && tecajnaListaItems.length > 0) {
+
+            String kupovniTecajString = tecajnaListaItems[0].getKupovniTecaj();
+            kupovniTecajString = kupovniTecajString.replace(",", ".");
+            BigDecimal kupovniTecaj = new BigDecimal(kupovniTecajString);
+            convertedPrice = price.multiply(kupovniTecaj);
+        }
+
+        return convertedPrice;
     }
 }
